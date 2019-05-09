f0c401efdd2005cfed4db054d89396a674a15163
Alexander Makarov <sam@rmcreative.ru>
2019-05-09 16:07:44 +0000

    Apply fixes from StyleCI

--- config/web.php
+++ config/web.php
@@ -2,10 +2,10 @@
 
 return [
     'app' => [
-        'controllerNamespace' =>  Yiisoft\Yii\Base\Api\Controllers::class,
+        'controllerNamespace' => Yiisoft\Yii\Base\Api\Controllers::class,
     ],
     'user' => [
-        'identityClass'   =>  Yiisoft\Yii\Base\Api\Models\User::class, // User must implement the IdentityInterface
+        'identityClass'   => Yiisoft\Yii\Base\Api\Models\User::class, // User must implement the IdentityInterface
         'enableAutoLogin' => false,
         'enableSession'   => false,
         'loginUrl'        => null,
